dfeb0688c1f447415f01dfedbe10f31d7991075f
GHA01SN <c20f118@outlook.com>
2018-05-28 10:52:27 +0800

    编写绘制功能

--- designs.js
+++ designs.js
@@ -6,5 +6,28 @@
 function makeGrid() {
 //绘制表格，大前提，设置色彩。
 // Your code goes here!
+    let height = $("#inputHeight").val();
+    let weight = $("#inputWeight").val();
 
+
+}
+
+function drawing(){
+    console.log("call the function makeGrid!");
+    $("td").on('click',function(){
+        console.log("td block clicked!");
+    })
+
+
+    let colorPicked = $('#colorPicker').val();
+    console.log(colorPicked);
+    $('#td_001').on('click',function(){
+        colorPicked = $('#colorPicker').val();
+
+        $("#td_001").css("backgroundColor",colorPicked);
+
+        console.log(colorPicked);
+        console.log("td block click! external output");
+        return false;
+    })
 }
