08572054849ca9741b2865a5d30cf328a06310a7
GHA01SN <c20f118@outlook.com>
2018-05-28 13:19:12 +0800

    获取url中参数

--- designs.js
+++ designs.js
@@ -5,29 +5,64 @@
 
 function makeGrid() {
 //绘制表格，大前提，设置色彩。
+//在绘制的过程中要将每一个id进行单独存储
+//动态的从数组中提取数据
 // Your code goes here!
     let height = $("#inputHeight").val();
     let weight = $("#inputWeight").val();
-
-
+    console.log(height);
+    console.log(weight);
+    getParam();
+    return false;
 }
 
+
+function getParam(){
+    //从url中获取参数
+    let siteUrl = location.href;
+    let arg_str = siteUrl.split("?");
+    let arg_container ;
+    let separate_arg ;
+    console.log("into getParam function block!");
+    if (arg_str.length === 2){
+        console.log(arg_str)
+        arg_container = arg_str[1];
+        console.log(arg_container);
+        separate_arg = arg_container.split("&");
+        console.log(separate_arg);
+
+        
+    }
+    let height , weight;
+    let height_index , weight_index;
+    for (let i = 0;i<separate_arg.length;i++){
+        let temp_container = separate_arg[i].split("=");
+        console.log(temp_container);
+        if(temp_container[0] === "height"){
+            height = temp_container[1];
+            console.log(height);
+        }else{
+            weight = temp_container[1];
+            console.log(weight);
+        }
+    }
+
+}
 function drawing(){
-    console.log("call the function makeGrid!");
+    let colorPicked = $('#colorPicker').val();
+    console.log("drawing begin!");
     $("td").on('click',function(){
         console.log("td block clicked!");
     })
+    let allTd = $("td");
 
-
-    let colorPicked = $('#colorPicker').val();
     console.log(colorPicked);
-    $('#td_001').on('click',function(){
-        colorPicked = $('#colorPicker').val();
+    
+    colorPicked = $('#colorPicker').val();
 
-        $("#td_001").css("backgroundColor",colorPicked);
+    $(".td1").css("backgroundColor",colorPicked);
 
-        console.log(colorPicked);
-        console.log("td block click! external output");
-        return false;
-    })
+    console.log(colorPicked);
+    console.log("td block click! external output");
+
 }
